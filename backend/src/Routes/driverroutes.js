5390e04aefb399d37298b01d206db24dba2c7eb5
Jitsu <sujitku641@gmail.com>
2025-11-30 17:42:21 +0530

    Add backendtest route to check if backend is awake

--- backend/src/Routes/driverroutes.js
+++ backend/src/Routes/driverroutes.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const driver = express.Router()
-const { driverprofile, findprofile ,loadreturn, availableload,availableBooking} = require("../Controllers/drivecontroller")
+const { driverprofile, findprofile ,loadreturn, availableload,availableBooking, BackendRun} = require("../Controllers/drivecontroller")
 
 driver.post("/profile", driverprofile)
 driver.post("/find", findprofile)
 driver.post("/returnload",loadreturn)
 driver.post("/availableload",availableload)
 driver.post("/booking",availableBooking)
+driver.get("/backendtest",BackendRun)
 
 
 
