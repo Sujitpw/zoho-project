fd0bb258d95b67a14c726e37dafb960ddb13c7a2
Sujit4141 <jitsuofficial41@gmail.com>
2025-11-30 18:00:42 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,13 @@ require('dotenv').config();
 const main=require("./config/db")
 const driverroutes=require("./Routes/driverroutes")
 
+const cors = require("cors");
+app.use(cors({
+  origin: "https://zohotruckmate.vercel.app", // allow only your frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/driver",driverroutes)
